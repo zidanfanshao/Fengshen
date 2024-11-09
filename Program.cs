3e7d9e90e3e3f0b02055cb41ca04bea28ea4a8db
zidanfanshao <zidaifanshao@gmail.com>
2024-11-10 00:23:19 +0800

    a

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Win32;
+
+class Fengshen
+{
+    static List<string> targetApps = new List<string>
+    {
+        "MobaXterm", "Xmanager", "RDCMan", "FinalShell", "Navicat",
+        "SQLyog", "SecureCRT", "Outlook", "MailBird", "WinSCP",
+        "DBeaver", "CoreFTP", "Snowflake", "HeidiSQL","foxmail"
+    };
+
+    static void Main()
+    {
+        foreach (var app in targetApps)
+        {
+            string installPath = FindApplicationInstallPath(app);
+            if (installPath != null)
+            {
+                Console.WriteLine($"[+] {app} install path: {installPath}.");
+            }
+            else
+            {
+                Console.WriteLine($"[-] {app} not install.");
+            }
+        }
+    }
+    static string FindApplicationInstallPath(string appName)
+    {
+        string installPath = null;
+
+        installPath = SearchRegistryForApp(appName);
+        if (installPath != null) return installPath;
+
+        installPath = CheckCommonInstallPaths(appName);
+        return installPath;
+    }
+
+    static string SearchRegistryForApp(string appName)
+    {
+        string[] registryPaths = {
+            @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+            @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+        };
+
+        foreach (var path in registryPaths)
+        {
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(path))
+            {
+                if (key != null)
+                {
+                    foreach (var subKeyName in key.GetSubKeyNames())
+                    {
+                        using (RegistryKey subKey = key.OpenSubKey(subKeyName))
+                        {
+                            var displayName = subKey.GetValue("DisplayIcon")?.ToString();
+                            if (displayName != null && displayName.Contains(appName, StringComparison.OrdinalIgnoreCase))
+                            {
+                                var installLocation = subKey.GetValue("DisplayIcon")?.ToString();
+                                if (!string.IsNullOrEmpty(installLocation))
+                                {
+                                    return installLocation;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    static string CheckCommonInstallPaths(string appName)
+    {
+        string[] possiblePaths = new string[]
+        {
+            @"C:\Program Files",
+            @"C:\Program Files (x86)"
+        };
+
+        Dictionary<string, string[]> appSubfolders = new Dictionary<string, string[]>
+        {
+            { "MobaXterm", new[] { "MobaXterm" } },
+            { "Xmanager", new[] { "Xmanager" } },
+            { "RDCMan", new[] { "RDCMan" } },
+            { "FinalShell", new[] { "FinalShell" } },
+            { "Navicat", new[] { "Navicat" } },
+            { "SQLyog", new[] { "SQLyog" } },
+            { "SecureCRT", new[] { "SecureCRT" } },
+            { "Outlook", new[] { "Microsoft Office", "Office" } },
+            { "MailBird", new[] { "Mailbird" } },
+            { "WinSCP", new[] { "WinSCP" } },
+            { "DBeaver", new[] { "DBeaver" } },
+            { "CoreFTP", new[] { "CoreFTP" } },
+            { "Snowflake", new[] { "Snowflake" } },
+            { "HeidiSQL", new[] { "HeidiSQL" } }
+        };
+
+        if (appSubfolders.ContainsKey(appName))
+        {
+            foreach (var rootPath in possiblePaths)
+            {
+                foreach (var subFolder in appSubfolders[appName])
+                {
+                    string fullPath = Path.Combine(rootPath, subFolder);
+                    if (Directory.Exists(fullPath))
+                    {
+                        return fullPath;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# Fengshen
+从注册表中查询是否安装某应用。
+
+## 目前支持查询以下应用
+*注：只查是否安装，以及安装目录，不解密*
+
+* MobaXterm, Xmanager, RDCMan, FinalShell, Navicat
+* SQLyog, SecureCRT, Outlook, MailBird, WinSCP
+* DBeaver, CoreFTP, Snowflake, HeidiSQL,foxmail
+
+
+
+# 嗯
+
+![image-20241110001402261](./assets/image-20241110001402261.png)
+
+
+
+
+# 运行
+
+单文件、自包含、设置 PublishTrimmed 去掉不必要的代码和依赖。
+```
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true
+
+// /p:PublishTrimmed=true：启用 发布修剪，它会去除没有使用的程序集和资源，从而减小文件大小。
+```
+
+
+
+# 运行结果
+
+```
+[-] MobaXterm not install.
+[-] Xmanager not install.
+[-] RDCMan not install.  
+[-] FinalShell not install.
+[-] Navicat not install.
+[-] SQLyog not install.
+[-] SecureCRT not install.
+[-] Outlook not install.
+[-] MailBird not install.
+[-] WinSCP not install.
+[-] DBeaver not install.
+[-] CoreFTP not install.
+[-] Snowflake not install.
+[-] HeidiSQL not install.
+[+] foxmail install path: C:\Foxmail 7.2\Foxmail.exe.
+```
+
